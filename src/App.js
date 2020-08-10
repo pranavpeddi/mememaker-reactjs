5cecf527341d95ee96ec0ae2f19fc4f5841120e1
pranav peddi <41251294+pranavpeddi@users.noreply.github.com>
2020-08-10 17:48:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import  'bootstrap/dist/css/bootstrap.min.css';
+
+import MainPage from  './Components/ind'
+
+
+
+class App extends Component
+{
+
+  render()
+  {
+    return(
+      <MainPage/>
+    )
+  }
+}
+export default App;
